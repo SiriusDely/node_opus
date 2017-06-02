48379115e4b650b3813237f68fcc7034afdbbe19
Sirius Dely <siriusdely@icloud.com>
2017-06-02 12:23:18 +0700

    using nan

--- hello.cc
+++ hello.cc
@@ -1,5 +1,6 @@
 // hello.cc
-#include <node.h>
+
+//#include <node.h>
 /*
 namespace demo {
 
@@ -24,16 +25,19 @@ NODE_MODULE(addon, init)
 }  // namespace demo
 */
 
-const int maxValue = 10;  
+#include <nan.h>
+
+const int maxValue = 10;
 int numberOfCalls = 0;
 
-void WhoAmI(const v8::FunctionCallbackInfo<v8::Value>& args) {  
+/*
+void WhoAmI(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
   auto message = v8::String::NewFromUtf8(isolate, "I'm a Node Hero!");
   args.GetReturnValue().Set(message);
 }
 
-void Increment(const v8::FunctionCallbackInfo<v8::Value>& args) {  
+void Increment(const v8::FunctionCallbackInfo<v8::Value>& args) {
   v8::Isolate* isolate = args.GetIsolate();
 
   if (!args[0]->IsNumber()) {
@@ -57,9 +61,44 @@ void Increment(const v8::FunctionCallbackInfo<v8::Value>& args) {
   args.GetReturnValue().Set(currentNumberOfCalls);
 }
 
-void Initialize(v8::Local<v8::Object> exports) {  
+void Initialize(v8::Local<v8::Object> exports) {
   NODE_SET_METHOD(exports, "whoami", WhoAmI);
   NODE_SET_METHOD(exports, "increment", Increment);
 }
 
-NODE_MODULE(module_name, Initialize)  
+NODE_MODULE(module_name, Initialize)
+*/
+
+void WhoAmI(const Nan::FunctionCallbackInfo<v8::Value>& args) {
+  auto message = Nan::New<v8::String>("I'm a Node Hero!").ToLocalChecked();
+  args.GetReturnValue().Set(message);
+}
+
+void Increment(const Nan::FunctionCallbackInfo<v8::Value>& args) {
+  if (!args[0]->IsNumber()) {
+    Nan::ThrowError("Argument must be a number");
+    return;
+  }
+
+  double argsValue = args[0]->NumberValue();
+  if (numberOfCalls + argsValue > maxValue) {
+    Nan::ThrowError("Counter went through the roof!");
+    return;
+  }
+
+  numberOfCalls += argsValue;
+
+  auto currentNumberOfCalls =
+    Nan::New<v8::Number>(numberOfCalls);
+
+  args.GetReturnValue().Set(currentNumberOfCalls);
+}
+
+void Initialize(v8::Local<v8::Object> exports) {
+  exports->Set(Nan::New("whoami").ToLocalChecked(),
+      Nan::New<v8::FunctionTemplate>(WhoAmI)->GetFunction());
+  exports->Set(Nan::New("increment").ToLocalChecked(),
+      Nan::New<v8::FunctionTemplate>(Increment)->GetFunction());
+}
+
+NODE_MODULE(addon, Initialize)
